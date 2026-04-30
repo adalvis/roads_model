1f8dad4e9505d7cff484503c07e58e47b95fda7c
adalvis <amanaste@uw.edu>
2026-04-30 13:31:30 -0600

    Updated to be more general; ability to make grid without ditch

--- utilities/erodible_grid.py
+++ utilities/erodible_grid.py
@@ -12,7 +12,9 @@ class Erodible_Grid(Component):
             ncols,
             spacing,
             full_tire,
-            long_slope
+            long_slope,
+            road_peak,
+            ditch=True
     ):
         """Initialize Erodible Grid Component"""
         super().__init__(None)
@@ -23,11 +25,11 @@ class Erodible_Grid(Component):
                                                         #node is part of the road or the ditch line
         n = mg.add_zeros('roughness', at='node') #create roughness field
             
-        mg.set_fixed_value_boundaries_at_grid_edges(True, True, True, True)     
+        # mg.set_open_boundaries_at_grid_edges(True, True, True, True)     
         mg.set_closed_boundaries_at_grid_edges(False, False, False, False) 
 
         if full_tire == False: #When node spacing is half-tire-width
-            road_peak = 40 #peak crowning height occurs at this x-location
+            road_peak = road_peak #peak crowning height occurs at this x-location
             up = 0.0067 #rise of slope from ditchline to crown
             down = 0.0067 #rise of slope from crown to fillslope
             
@@ -35,33 +37,44 @@ class Erodible_Grid(Component):
                 elev = 0 #initialize elevation placeholder
                 flag = False #initialize road_flag placeholder
 
-                for h in range(ncols): #loop through road width
-                    if h == 0 or h == 8:
-                        elev = 0
-                        flag = False
-                    elif h == 1 or h == 7:
-                        elev = -0.109
-                        flag = False
-                    elif h == 2 or h == 6:
-                        elev = -0.1875
-                        flag = False
-                    elif h == 3 or h == 5:
-                        elev = -0.2344
-                        flag = False
-                    elif h == 4:
-                        elev = -0.25
-                        flag = False
-                    elif h <= road_peak and h > 8: #update latitudinal slopes based on location related to road_peak
-                        elev += up
-                        flag = True
-                    else:
-                        elev -= down
-                        flag = True
+                if ditch==True:
+                    for h in range(ncols): #loop through road width
+                        if h == 0 or h == 8:
+                            elev = 0
+                            flag = False
+                        elif h == 1 or h == 7:
+                            elev = -0.109
+                            flag = False
+                        elif h == 2 or h == 6:
+                            elev = -0.1875
+                            flag = False
+                        elif h == 3 or h == 5:
+                            elev = -0.2344
+                            flag = False
+                        elif h == 4:
+                            elev = -0.25
+                            flag = False
+                        elif h <= road_peak and h > 8: #update latitudinal slopes based on location related to road_peak
+                            elev += up
+                            flag = True
+                        else:
+                            elev -= down
+                            flag = True
+                        z[g*ncols + h] = elev #update elevation based on x & y locations
+                        road_flag[g*ncols+h] = flag #update road_flag based on x & y locations
+                else:
+                    for h in range(ncols):
+                        if h <= road_peak:
+                            elev += up
+                            flag = True
+                        else:
+                            elev -= down
+                            flag = True
 
-                    z[g*ncols + h] = elev #update elevation based on x & y locations
-                    road_flag[g*ncols+h] = flag #update road_flag based on x & y locations
+                        z[g*ncols + h] = elev #update elevation based on x & y locations
+                        road_flag[g*ncols+h] = flag #update road_flag based on x & y locations
         elif full_tire == True: #When node spacing is full-tire-width
-            road_peak = 20 #peak crowning height occurs at this x-location
+            road_peak = road_peak #peak crowning height occurs at this x-location
             up = 0.0134 #rise of slope from ditchline to crown
             down = 0.0134 #rise of slope from crown to fillslope
             
@@ -69,25 +82,35 @@ class Erodible_Grid(Component):
                 elev = 0 #initialize elevation placeholder
                 flag = False #initialize road_flag placeholder
 
-                for h in range(ncols): #loop through road width
-                    if h == 0 or h == 4:
-                        elev = 0
-                        flag = False
-                    elif h == 1 or h == 3:
-                        elev = -0.1875
-                        flag = False
-                    elif h == 2:
-                        elev = -0.25
-                        flag = False
-                    elif h <= road_peak and h > 4: #update latitudinal slopes based on location related to road_peak
-                        elev += up
-                        flag = True
-                    else:
-                        elev -= down
-                        flag = True
-
-                    z[g*ncols + h] = elev #update elevation based on x & y locations
-                    road_flag[g*ncols+h] = flag #update road_flag based on x & y locations
+                if ditch==True:
+                    for h in range(ncols): #loop through road width
+                        if h == 0 or h == 4:
+                            elev = 0
+                            flag = False
+                        elif h == 1 or h == 3:
+                            elev = -0.1875
+                            flag = False
+                        elif h == 2:
+                            elev = -0.25
+                            flag = False
+                        elif h <= road_peak and h > 4: #update latitudinal slopes based on location related to road_peak
+                            elev += up
+                            flag = True
+                        else:
+                            elev -= down
+                            flag = True
+                        z[g*ncols + h] = elev #update elevation based on x & y locations
+                        road_flag[g*ncols+h] = flag #update road_flag based on x & y locations
+                else:
+                    for h in range(ncols): #loop through road width
+                        if h <= road_peak:
+                            elev += up
+                            flag = True
+                        else:
+                            elev -= down
+                            flag = True
+                        z[g*ncols + h] = elev #update elevation based on x & y locations
+                        road_flag[g*ncols+h] = flag #update road_flag based on x & y locations
         
         z += mg.node_y*long_slope #add longitudinal slope to road segment
         road_flag = road_flag.astype(bool) #Make sure road_flag is a boolean array
