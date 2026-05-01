d275e51cb40a299d63706db412625abbbe83199e
Amanda Alvis <amanaste@uw.edu>
2026-05-01 13:39:10 -0600

    Testing with rainfall data; realized fraction formulation is incorrect-need to update

--- oft_testing_noDitch.py
+++ oft_testing_noDitch.py
@@ -11,7 +11,7 @@ np.set_printoptions(threshold=np.inf)
 from utilities.erodible_grid import Erodible_Grid
 
 # %% Define parameters
-run_duration = 90 
+run_duration = 122
 
 #Physical constants
 rho_w=1000
@@ -19,11 +19,11 @@ rho_s=2650
 g=9.81
 
 #Define site
-site_name = "MEL05"
+site_name = "MEL12"
 rain_gauge_list = ["RG_BISH05_mm","RG_BISH12_mm","RG_DEL0103_mm",\
     "RG_KID1316_mm","RG_KID46_mm","RG_MEL05_mm","RG_MEL14_mm",\
     "RG_NASE0104_mm","RG_NASE05i_mm","RG_NEWS1920_mm"]
-rain_gauge = rain_gauge_list[5]
+rain_gauge = rain_gauge_list[6]
 
 #Call parameters from .csv file
 parameters = pd.read_csv("input/parameters_WY2024.csv")
@@ -32,24 +32,24 @@ S = site_params["Road Gradient"]/100
 porosity = 0.35
 
 #Define index to start model run from
-intensity_index = 144
+intensity_index = 62
 
 seed = 1 
 np.random.seed(seed)
 
 #Intensity data in mm/hr
-# intensity = pd.read_csv("input/WY2024_RG_daily_intensity.csv")
-intensity = np.random.choice([0,4],size=(run_duration),p=[0.4,0.6])
+intensity = pd.read_csv("input/WY2024_RG_daily_intensity.csv")
+# intensity = np.random.choice([0,4],size=(run_duration),p=[0.4,0.6])
 
 #Change the site name and index values to get different sites and dates
-# intensity_90 = intensity[rain_gauge].iloc[intensity_index:].values
-intensity_90 = intensity.copy()
+intensity_90 = intensity[rain_gauge].iloc[intensity_index:].values
+# intensity_90 = intensity.copy()
 
 #Daily storm duration in hours
-# dt_hours = pd.read_csv("input/WY2024_RG_daily_dt.csv")
-# dt_hours_90 = dt_hours[rain_gauge].iloc[high_intensity_index:].values 
-dt_hours = np.array([4 if x==4 else 0 for x in intensity])
-dt_hours_90 = dt_hours.copy()
+dt_hours = pd.read_csv("input/WY2024_RG_daily_dt.csv")
+dt_hours_90 = dt_hours[rain_gauge].iloc[intensity_index:].values 
+# dt_hours = np.array([4 if x==4 else 0 for x in intensity])
+# dt_hours_90 = dt_hours.copy()
 #Convert to dt to days
 dt = np.array(dt_hours_90)/24
 
@@ -66,6 +66,7 @@ n_c = 0.05
 n_f = 0.015
 
 # fractions of fine and coarse grains in the active layer
+# FIX THIS FORMULATION
 f_af = 0.75
 f_ac = 0.25
 
@@ -435,9 +436,12 @@ for i in range(0, run_duration): # daily time step
         sa_arr[i] = np.sum(mg.at_node['active__depth'][full_road])
         ss_arr[i] = np.sum(mg.at_node['surfacing__depth'][full_road])
         sb_arr[i] = np.sum(mg.at_node['ballast__depth'][full_road])
-
-    print("Change in sum of depths:", (active_init.sum()+surfacing_init.sum()+ballast_init.sum()) - (sa_arr[i]+ss_arr[i]+sb_arr[i]))
-    print("Change in z from OFT:", np.divide(total_road_mass.cumsum()[i], cell_area*rho_s*(1-porosity)))
+    if i == 92-62 or i == 123-62 or i == 152-62 or i == 183-62:
+        print("End of month total:", np.round((mass_ditch_inflow + mass_ditch_rut_outflow).sum(),2), "kg" )
+    print("Change in sum of depths:", np.round((active_init.sum()+surfacing_init.sum()+ballast_init.sum())\
+         - (sa_arr[i]+ss_arr[i]+sb_arr[i]),2))
+    print("Change in z from OFT:", np.round(np.divide(total_road_mass.cumsum()[i],\
+         cell_area*rho_s*(1-porosity)),2))
 wall_time = time.time() - start
 print("Wall time for run:", wall_time, "s")
 
@@ -520,7 +524,7 @@ plt.plot(X[36], xsec_pre, color='gray', linestyle='-.', label='Before')
 plt.plot(X[36], xsec_active, color ='black', linestyle='-', label = 'After - Active elevation')
 plt.plot(X[36], xsec_surf, color ='magenta', linestyle='-', label = 'After - Surfacing elevation')
 plt.plot(X[36], xsec_ball, color ='cyan', linestyle='-', label = 'After - Ballast elevation ')
-plt.xlim(0,10)
+plt.xlim(0,9.5)
 plt.xlabel('Road width (m)')
 plt.ylabel('Elevation (m)')
 plt.legend()
@@ -622,10 +626,10 @@ plt.tight_layout()
 plt.show()
 
 # %% Testing?
-plt.plot(np.multiply(sa_arr,(cell_area*rho_s*(1-porosity)))/2, -cum_road_mass_change_oft)
-plt.xlabel("Active depth mass\nhalf road [$kg$]")
-plt.ylabel('Cumulative mass change (from OFT)\nhalf road [$kg$]')
-plt.show()
+# plt.plot(np.multiply(sa_arr,(cell_area*rho_s*(1-porosity)))/2, -cum_road_mass_change_oft)
+# plt.xlabel("Active depth mass\nhalf road [$kg$]")
+# plt.ylabel('Cumulative mass change (from OFT)\nhalf road [$kg$]')
+# plt.show()
 # %% TPE loading
 # plot sediment load to the active layer in the ruts from truck passes
 plt.plot(range(0,run_duration), tpe_load_ruts)
@@ -635,47 +639,55 @@ plt.xlim(0,run_duration)
 plt.show()
 
 # %% Total depths over the road surface 
-plt.plot(range(0,run_duration), sa_arr)
-plt.xlabel('Day')
-plt.ylabel('Active Depth over full road surface [$m$]')
-plt.xlim(0,run_duration)
-plt.title(r'%s ($n_{f_{road}} = %0.3f$)' %(site_name, n_f))
-plt.show()
+fig, ax = plt.subplots(4,1, figsize=(4,9))
 
-plt.plot(range(0,run_duration), ss_arr)
-plt.xlabel('Day')
-plt.ylabel('Surfacing Depth over full road surface [$m$]')
-plt.xlim(0,run_duration)
-plt.show()
+active_init.sum()+surfacing_init.sum()+ballast_init.sum()
 
-plt.plot(range(0,run_duration), sb_arr)
-plt.xlabel('Day')
-plt.ylabel('Ballast Depth over full road surface [$m$]')
-plt.xlim(0,run_duration)
+ax[0].plot(range(0,run_duration), (-active_init.sum()-surfacing_init.sum() + (sa_arr+ss_arr))/(nrows*ncols))
+ax[0].set_xlabel('Day')
+ax[0].set_ylabel('Active + Surfacing Depth\nchange [$m$]')
+ax[0].set_xlim(0,run_duration)
+ax[0].set_title(r'%s ($n_{f_{road}} = %0.3f$)' %(site_name, n_f))
+
+ax[1].plot(range(0,run_duration), (-active_init.sum()+sa_arr)/(nrows*ncols))
+ax[1].set_xlabel('Day')
+ax[1].set_ylabel('Active Depth\nchange [$m$]')
+ax[1].set_xlim(0,run_duration)
+
+ax[2].plot(range(0,run_duration), (-surfacing_init.sum()+ss_arr)/(nrows*ncols))
+ax[2].set_xlabel('Day')
+ax[2].set_ylabel('Surfacing Depth\nchange [$m$]')
+ax[2].set_xlim(0,run_duration)
+
+ax[3].plot(range(0,run_duration), (-ballast_init.sum()+sb_arr)/(nrows*ncols))
+ax[3].set_xlabel('Day')
+ax[3].set_ylabel('Ballast Depth\nchange [$m$]')
+ax[3].set_xlim(0,run_duration)
+plt.tight_layout()
 plt.show()
 
 # %%
-plt.bar(range(0,run_duration), np.multiply(road_shear_frac_arr, 100))
-plt.xlabel('Day')
-plt.ylabel('Percentage of Road $\\geq$ Critical \nShear Stress')
-plt.xlim(0,run_duration)
-plt.title(r'%s' %site_name)
-plt.show()
+# plt.bar(range(0,run_duration), np.multiply(road_shear_frac_arr, 100))
+# plt.xlabel('Day')
+# plt.ylabel('Percentage of Road $\\geq$ Critical \nShear Stress')
+# plt.xlim(0,run_duration)
+# plt.title(r'%s' %site_name)
+# plt.show()
 
-plt.plot(range(0,run_duration), np.multiply(road_shear_cum_arr, 100))
-plt.xlabel('Day')
-plt.ylabel('Cumulative Percentage of Road $\\geq$ Critical \nShear Stress')
-plt.xlim(0,run_duration)
-plt.title(r'%s' %site_name)
-plt.show()
+# plt.plot(range(0,run_duration), np.multiply(road_shear_cum_arr, 100))
+# plt.xlabel('Day')
+# plt.ylabel('Cumulative Percentage of Road $\\geq$ Critical \nShear Stress')
+# plt.xlim(0,run_duration)
+# plt.title(r'%s' %site_name)
+# plt.show()
 
-# plot shear stress partitioning coefficient over time
-plt.plot(range(0,run_duration), fs_avg_road, label='Full Road')
-plt.plot(range(0,run_duration), fs_avg_ruts, label='Ruts')
-plt.xlabel('Day')
-plt.ylabel('Average Shear Stress Partitioning \nCoefficient: $f_s$')
-plt.xlim(0,run_duration)
-plt.legend()
-plt.show()
+# # plot shear stress partitioning coefficient over time
+# plt.plot(range(0,run_duration), fs_avg_road, label='Full Road')
+# plt.plot(range(0,run_duration), fs_avg_ruts, label='Ruts')
+# plt.xlabel('Day')
+# plt.ylabel('Average Shear Stress Partitioning \nCoefficient: $f_s$')
+# plt.xlim(0,run_duration)
+# plt.legend()
+# plt.show()
 
 # %%
